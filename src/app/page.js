bfb6cc24f832e42ddb3f6b9b9e7812a7e9dae721
Alan Douglas Aranda <alandouglasaranda@Alans-MacBook-Pro-2.local>
2023-04-21 19:24:08 +0200

    First section WIP

--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,91 @@
-'use client'
-
-import { useRef, useState } from 'react'
+import Animation from '../components/animation_v1'
 import styles from './page.module.css'
 
-const DEG_ROTATION = 25
-
 export default function Home() {
-  const anchorRef = useRef()
-
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-
-  function handleMove(e) {
-    const mouseX = e.clientX
-    const mouseY = e.clientY
-
-    const rekt = anchorRef.current.getBoundingClientRect()
-
-    setPosition({ x: mouseX - rekt.x, y: mouseY - rekt.y })
-  }
-
   return (
-    <main className={styles.main} onMouseMove={handleMove}>
-      <div
-        style={{
-          transformStyle: 'preserve-3d',
-          height: '300px',
-          width: '300px',
-          transform: `rotateX(${position.y / -DEG_ROTATION}deg) rotateY(${
-            position.x / DEG_ROTATION
-          }deg)`,
-        }}
-      >
-        <div ref={anchorRef} className={styles.element1}></div>
+    <main className={styles.main}>
+      <section className={styles.section1}>
         <div
-          className={styles.element2}
           style={{
-            transform: `translateZ(40px)`,
-          }}
-        ></div>
-        <div
-          className={styles.element3}
-          style={{
-            transform: `translateZ(40px)`,
+            display: 'flex',
+            width: '90%',
+            paddingTop: '150px',
+            margin: 'auto',
           }}
         >
-          ❤️
+          <div className={styles.element1}>
+            <h1 className={styles.h1}>
+              Everything you are. In one, simple link in bio.
+            </h1>
+            <p className={styles.p}>
+              Join 35M+ people using Linktree for their link in bio. One link to
+              help you share everything you create, curate and sell from your
+              Instagram, TikTok, Twitter, YouTube and other social media
+              profiles.
+            </p>
+            <form style={{ display: 'flex', gap: '12px' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  height: '62px',
+                  width: '243px',
+                  backgroundColor: 'white',
+                  borderRadius: '8px',
+                  padding: '19px 16px',
+                  color: 'rgb(103, 107, 95)',
+                  fontFamily: 'Arial, sans-serif',
+                  fontSize: '18px',
+                }}
+              >
+                <p
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  linktr.ee/
+                </p>
+                <p
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <input
+                    type="text"
+                    placeholder="yourname"
+                    style={{
+                      border: '0px',
+                      outline: '0px',
+                      fontSize: '18px',
+                      width: '70%',
+                    }}
+                  />
+                </p>
+              </div>
+              <div>
+                <button
+                  style={{
+                    width: '212px',
+                    height: '62px',
+                    backgroundColor: 'rgb(233, 192, 233)',
+                    border: '0px',
+                    outline: '0px',
+                    fontSize: '18px',
+                    color: 'rgb(30, 35, 48)',
+                    borderRadius: '31px',
+                  }}
+                >
+                  Claim your Linktree
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className={styles.element2}>
+            <Animation />
+          </div>
         </div>
-      </div>
+      </section>
     </main>
   )
 }
---
+++ src/components/animation_v1.js
@@ -0,0 +1,53 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import styles from './animation_v1.module.css'
+
+const DEG_ROTATION = 25
+
+export default function Animation() {
+  const anchorRef = useRef()
+
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  function handleMove(e) {
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const rekt = anchorRef.current.getBoundingClientRect()
+
+    setPosition({ x: mouseX - rekt.x, y: mouseY - rekt.y })
+  }
+
+  return (
+    // TESTINGAL -> Use hook for mouseTracker
+    <div className={styles.main} onMouseMove={handleMove}>
+      <div
+        style={{
+          transformStyle: 'preserve-3d',
+          height: '300px',
+          width: '300px',
+          transform: `rotateX(${position.y / -DEG_ROTATION}deg) rotateY(${
+            position.x / DEG_ROTATION
+          }deg)`,
+        }}
+      >
+        <div ref={anchorRef} className={styles.element1}></div>
+        <div
+          className={styles.element2}
+          style={{
+            transform: `translateZ(40px)`,
+          }}
+        ></div>
+        <div
+          className={styles.element3}
+          style={{
+            transform: `translateZ(40px)`,
+          }}
+        >
+          ❤️
+        </div>
+      </div>
+    </div>
+  )
+}
