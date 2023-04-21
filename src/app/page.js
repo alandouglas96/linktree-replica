76d9c2e4c919b3633bc8039835f645c48efc5f86
Alan Douglas Aranda <alandouglasaranda@Alans-MacBook-Pro-2.local>
2023-04-21 13:56:47 +0200

    First working implementation for animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,51 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+'use client'
+
+import { useRef, useState } from 'react'
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const DEG_ROTATION = 25
 
 export default function Home() {
+  const anchorRef = useRef()
+
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  function handleMove(e) {
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const rekt = anchorRef.current.getBoundingClientRect()
+
+    setPosition({ x: mouseX - rekt.x, y: mouseY - rekt.y })
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.main} onMouseMove={handleMove}>
+      <div
+        style={{
+          transformStyle: 'preserve-3d',
+          height: '300px',
+          width: '300px',
+          transform: `rotateX(${position.y / -DEG_ROTATION}deg) rotateY(${
+            position.x / DEG_ROTATION
+          }deg)`,
+        }}
+      >
+        <div ref={anchorRef} className={styles.element1}></div>
+        <div
+          className={styles.element2}
+          style={{
+            transform: `translateZ(40px)`,
+          }}
+        ></div>
+        <div
+          className={styles.element3}
+          style={{
+            transform: `translateZ(40px)`,
+          }}
+        >
+          ❤️
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
